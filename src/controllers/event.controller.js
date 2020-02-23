c65dc2f9c025a2c39d27e45db61f366f1474f957
jchung201 <jchung201@gmail.com>
2020-02-23 13:03:46 -0500

    (Event) Post route.

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -7,6 +7,39 @@ import auth from "../middlewares/auth";
 
 const EVENT = mongoose.model("EVENT");
 
+// Make event with your ID stuff
+router.post(
+  "/",
+  auth,
+  asyncHandler(async (req, res, next) => {
+    const {
+      category,
+      description,
+      start,
+      end,
+      location,
+      title,
+      picture
+    } = req.body;
+    const newEvent = new EVENT({
+      vendor: req.user._id,
+      category,
+      description,
+      date: {
+        start,
+        end
+      },
+      location,
+      title,
+      picture
+    });
+    const savedEvent = await newEvent.save();
+    res.send({
+      event: savedEvent
+    });
+  })
+);
+
 // Get personal user profile
 router.get(
   "/",
