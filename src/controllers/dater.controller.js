d76e58a35642552fa26dd84e61e2bbc3729bf241
jchung201 <jchung201@gmail.com>
2020-02-23 01:43:03 -0500

    (User) patch routes.

--- src/controllers/dater.controller.js
+++ src/controllers/dater.controller.js
@@ -8,58 +8,79 @@ const USER = mongoose.model("USER");
 const MATCH = mongoose.model("MATCH");
 
 // Get personal user profile
-router.get(
-  "/me",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    res.send({
-      user: req.user
-    });
-  })
-);
-// Get propspects
-router.get(
-  "/prospects",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    const matches = await MATCH.find({ users: req.user._id });
-    const users = await USER.find({ _id: { $ne: req.user._id } });
-    // Return prospects that are not a match yet
-    // This ensures that there are no duplicates
-    // TODO: Include filtered information based on location and stuff
-    res.send({
-      prospects: users.filter(user => {
-        for (let i = 0; i < matches.length; i++) {
-          if (matches[i].users.includes(user._id)) {
-            return false;
+router
+  .get(
+    "/me",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      res.send({
+        user: req.user
+      });
+    })
+  )
+  // Get propspects
+  .get(
+    "/prospects",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const matches = await MATCH.find({ users: req.user._id });
+      const users = await USER.find({ _id: { $ne: req.user._id } });
+      // Return prospects that are not a match yet
+      // This ensures that there are no duplicates
+      // TODO: Include filtered information based on location and stuff
+      res.send({
+        prospects: users.filter(user => {
+          for (let i = 0; i < matches.length; i++) {
+            if (matches[i].users.includes(user._id)) {
+              return false;
+            }
           }
-        }
-        return true;
-      })
-    });
-  })
-);
-// Get matches
-router.get(
-  "/matches",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    // Return only matched 'MATCH's
-    res.send({
-      matches: await MATCH.find({ users: req.user._id, status: "matched" })
-    });
-  })
-);
-// Get RSVPS
-router.get(
-  "/rsvps",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    // Return only going rsvps
-    res.send({
-      rsvps: await RSVP.find({ user: req.user._id, status: "going" })
-    });
-  })
-);
+          return true;
+        })
+      });
+    })
+  )
+  // Get matches
+  .get(
+    "/matches",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      // Return only matched 'MATCH's
+      res.send({
+        matches: await MATCH.find({ users: req.user._id, status: "matched" })
+      });
+    })
+  )
+  // Get RSVPS
+  .get(
+    "/rsvps",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      // Return only going rsvps
+      res.send({
+        rsvps: await RSVP.find({ user: req.user._id, status: "going" })
+      });
+    })
+  );
+
+// Edit filters
+router
+  .patch(
+    "/filters",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const { distance, ageLow, ageHigh, heightLow, heightHigh } = req.body;
+      res.send({ user: "New" });
+    })
+  )
+  // Edit profile information
+  .patch(
+    "/profile",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const { name, birthDate, location, description } = req.body;
+      res.send({ user: "New" });
+    })
+  );
 
 export default router;
