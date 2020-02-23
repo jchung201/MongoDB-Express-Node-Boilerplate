bae2db17dc494fbaa8fe31a363a4e9d8519bf97a
jchung201 <jchung201@gmail.com>
2020-02-23 13:10:40 -0500

    (RSVP) Single fetch and update.

--- src/controllers/rsvp.controller.js
+++ src/controllers/rsvp.controller.js
@@ -26,23 +26,43 @@ router.post(
 );
 
 // Get personal user profile
-router.get(
-  "/",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    // TODO: Filter rsvps
-    res.send({ rsvps: await RSVP.find({ user: req.user._id }) });
-  })
-);
+router
+  .get(
+    "/",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      // TODO: Filter rsvps
+      res.send({ rsvps: await RSVP.find({ user: req.user._id }) });
+    })
+  )
+  .get(
+    "/:id",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const { id } = req.params;
+      res.send({ rsvp: await RSVP.findById(id) });
+    })
+  );
 
-// Find user profile
+// Edit RSVP
 router.patch(
   "/:id",
   auth,
   asyncHandler(async (req, res, next) => {
     const { id } = req.params;
-    // TODO: Edit event attendance
-    res.send();
+    const { status } = req.body;
+    const foundRSVP = await RSVP.findById(id);
+    if (!foundRSVP) throw httpErrors(401, "RSVP not found!");
+    if (!foundRSVP.user.equals(req.user._id))
+      throw httpErrors(401, "This is not your RSVP!");
+    const updatedRSVP = await RSVP.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+    res.send({
+      rsvp: updatedRSVP
+    });
   })
 );
 
