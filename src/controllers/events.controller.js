449ed99eee04cc71b8340b42eb583a350d650eb1
jchung201 <jchung201@gmail.com>
2020-02-23 00:45:10 -0500

    (Controllers) Created for new models.

---
+++ src/controllers/events.controller.js
@@ -0,0 +1,32 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import httpErrors from "http-errors";
+const router = express.Router();
+import mongoose from "mongoose";
+import auth from "../middlewares/auth";
+
+const USER = mongoose.model("USER");
+
+// Get personal user profile
+router.get(
+  "/me",
+  auth,
+  asyncHandler(async (req, res, next) => {
+    res.send({
+      user: req.user
+    });
+  })
+);
+
+// Find user profile
+router.get(
+  "/:id",
+  auth,
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    // Filter users
+    res.send({ user: await USER.findById(id) });
+  })
+);
+
+export default router;
--- src/library/router.js
+++ src/library/router.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 import authController from "../controllers/auth.controller";
 router.use("/api/auth", authController);
+import eventsController from "../controllers/events.controller";
+router.use("/api/events", eventsController);
+import matchesController from "../controllers/matches.controller";
+router.use("/api/matches", matchesController);
+import rsvpsController from "../controllers/rsvps.controller";
+router.use("/api/rsvps", rsvpsController);
 import usersController from "../controllers/users.controller";
 router.use("/api/users", usersController);
 
