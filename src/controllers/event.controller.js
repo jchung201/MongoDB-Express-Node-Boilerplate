7777a16f0603a5481f0906a94739062fb3458a34
jchung201 <jchung201@gmail.com>
2020-02-23 13:26:28 -0500

    (Event) Patch route.

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -41,12 +41,61 @@ router.post(
 );
 
 // Get personal user profile
-router.get(
-  "/",
+router
+  .get(
+    "/",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      // TODO: Filter events
+      res.send({ events: await EVENT.find({}) });
+    })
+  )
+  .get(
+    "/:id",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const { id } = req.params;
+      res.send({ event: await EVENT.findById(id) });
+    })
+  );
+
+// Edit EVENT
+router.patch(
+  "/:id",
   auth,
   asyncHandler(async (req, res, next) => {
-    // TODO: Filter events
-    res.send({ events: await EVENT.find({}) });
+    const { id } = req.params;
+    const {
+      category,
+      description,
+      start,
+      end,
+      location,
+      title,
+      picture
+    } = req.body;
+    const foundEvent = await EVENT.findById(id);
+    if (!foundEvent) throw httpErrors(401, "Event not found!");
+    if (!foundEvent.vendor.equals(req.user._id))
+      throw httpErrors(401, "This is not your Event!");
+    const updatedEvent = await EVENT.findByIdAndUpdate(
+      id,
+      {
+        category: category || foundEvent.category,
+        description: description || foundEvent.description,
+        date: {
+          start: start || foundEvent.date.start,
+          end: end || foundEvent.date.end
+        },
+        location: location || foundEvent.location,
+        title: title || foundEvent.title,
+        picture: picture || foundEvent.picture
+      },
+      { new: true }
+    );
+    res.send({
+      event: updatedEvent
+    });
   })
 );
 
