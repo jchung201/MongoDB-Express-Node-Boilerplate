5a8ec014c2881b3917ada0469a5b331527b09137
jchung201 <jchung201@gmail.com>
2020-02-24 09:53:32 -0500

    (Vendor) Fetch events.

--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -2,19 +2,31 @@ import express from "express";
 import asyncHandler from "express-async-handler";
 const router = express.Router();
 import httpErrors from "http-errors";
+import mongoose from "mongoose";
 
 import auth from "../middlewares/auth";
+const USER = mongoose.model("USER");
+const EVENT = mongoose.model("EVENT");
 
 // Get personal user profile
-router.get(
-  "/:id",
-  auth,
-  asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-    const foundUser = await USER.findById(id);
-    if (!foundUser) throw httpErrors(401, "Vendor does not exist!");
-    res.send({ user: foundUser });
-  })
-);
+router
+  .get(
+    "/events",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const foundEvents = EVENT.find({ vendor: req.user._id });
+      res.send({ events: foundEvents });
+    })
+  )
+  .get(
+    "/:id",
+    auth,
+    asyncHandler(async (req, res, next) => {
+      const { id } = req.params;
+      const foundUser = await USER.findById(id);
+      if (!foundUser) throw httpErrors(401, "Vendor does not exist!");
+      res.send({ user: foundUser });
+    })
+  );
 
 export default router;
