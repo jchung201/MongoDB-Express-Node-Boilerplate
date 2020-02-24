9ce2100b1c1619dd0dbb500d94257d8c4c9253bd
jchung201 <jchung201@gmail.com>
2020-02-24 10:02:27 -0500

    (Email) Template setup.:

---
+++ src/utilities/email.js
@@ -0,0 +1,21 @@
+// require("dotenv").config(); // process.env
+// const sgMail = require("@sendgrid/mail");
+// sgMail.setApiKey(process.env.SENDGRID);
+
+// const notificationEmail = (email, subject, templateId) => {
+//   const message = {
+//     to: email,
+//     from: "noreply@letzb.com",
+//     templateId,
+//     dynamic_template_data: {
+//       // Other dynamic template data
+//       subject
+//     },
+//     hideWarnings: true
+//   };
+//   sgMail.send(message);
+// };
+
+// module.exports = {
+//   notificationEmail
+// };
