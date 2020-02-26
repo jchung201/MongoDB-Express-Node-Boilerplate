ef2f92d024466c45b1e363079efb7d0b7c2bf4d3
jchung201 <jchung201@gmail.com>
2020-02-25 21:42:54 -0500

    (User) Pre save encrypt and fields

--- src/models/user/user.schema.js
+++ src/models/user/user.schema.js
@@ -22,32 +22,37 @@ const userSchema = new Schema(
       name: String,
       birthDate: Date,
       location: String,
-      description: String,
       photos: [
         {
           url: String,
           order: Number
         }
       ],
-      questions: {
-        step: { type: Number, default: 0 },
-        meet: [String],
+      icons: {
         horoscope: String,
-        schedule: [String],
-        searching: [String],
-        firstDate: [String],
-        bodyType: [String],
-        loveLanguage: [String],
-        lifeStyle: [String],
-        sexualPreference: [String],
-        relationshipStatus: String,
-        genderIdentity: String,
-        tidbit: String
+        life: String,
+        love: String,
+        meetMe: String,
+        preference: String,
+        schedule: String,
+        search: String,
+        sexuality: String,
+        status: String,
+        tell: String
       }
     }
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function(next) {
+  if (this.isNew) {
+    this.auth.email = this.auth.email.toLowerCase();
+    this.encryptPassword(this.auth.password, next);
+  } else {
+    return next();
+  }
+});
 
 // Require Methods
 require("./user.methods")(userSchema);
